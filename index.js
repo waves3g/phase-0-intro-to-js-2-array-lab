2b4d9d2d301eabe6a0cb4374cbc6245e09bef55a
Courtney Garcia <wavesofgrace3@gmail.com>
2022-04-06 10:37:54 -0700

    commit tree 6dc5a5a8972f834449b76e46121626407019210e

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(){
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(){
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop('Garfield');
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift('Milo');
+}
+
+function appendCat(name){
+    const newCats = [...cats, name];
+    return newCats;
+   }
+
+function prependCat(name){
+    const newCats2 = [name,...cats];
+    return newCats2;
+}
+
+function removeLastCat(){
+    const newCats3 = cats.slice(0, cats.length - 1)
+    return newCats3;
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
